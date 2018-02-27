625090a9d48330a10f1ec6ca76fc45ba5aec1e4b
Noah Smith <noahsmith@interapts-macbook-pro-3.local>
2018-02-27 13:48:10 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Noah Smith on 2/27/18.
+//  Copyright © 2018 Noah Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
